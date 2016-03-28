e7d84dae78cba17d5860b033d23a0e67c5a5a6d4
taoyuan <torworx@gmail.com>
2016-03-28 18:11:57 +0800

    change npos-tesseract to optional for windows

--- test/commands/mon.test.js
+++ test/commands/mon.test.js
@@ -5,12 +5,15 @@ var fs = require('fs');
 var npos = require('npos');
 var s = require('../support');
 var mon = require('../../lib/commands/mon');
+var logger = require('../../lib/logger');
 
 describe('mon', function () {
   it('should translate image to text', function () {
     var raw = fs.readFileSync(s.fixtures('raster.bin'));
-    var parser = npos.parser().use(mon.translate());
+    var parser = npos.parser().use(mon.translate({ocr: true}));
+    logger.profile('data');
     return parser.parse(raw).then(function (result) {
+      logger.profile('data');
       console.log(result.text);
       assert.include(result.text, '单号');
     });
