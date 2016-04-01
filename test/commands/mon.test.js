5d3ee697a2a69bc80531e293597875d57cac0e2c
taoyuan <torworx@gmail.com>
2016-04-01 16:13:35 +0800

    Update

--- test/commands/mon.test.js
+++ test/commands/mon.test.js
@@ -2,6 +2,7 @@
 
 var assert = require('chai').assert;
 var fs = require('fs');
+var path = require('path');
 var npos = require('npos');
 var s = require('../support');
 var mon = require('../../lib/commands/mon');
@@ -10,12 +11,15 @@ var logger = require('../../lib/logger');
 describe('mon', function () {
   it('should translate image to text', function () {
     var raw = fs.readFileSync(s.fixtures('raster.bin'));
-    var parser = npos.parser().use(mon.translate({ocr: true}));
+    var parser = npos.parser().use(mon.translate({
+      tessdata: path.join(__dirname, '..', 'fixtures', 'tessdata'),
+      language: 'pos.chs.fast'
+    }));
     logger.profile('data');
     return parser.parse(raw).then(function (result) {
       logger.profile('data');
       console.log(result.text);
-      assert.include(result.text, '单号');
+      assert.include(result.text, '现金');
     });
   });
 });
